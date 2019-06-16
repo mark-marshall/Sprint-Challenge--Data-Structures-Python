eb8a76483152bc242d4a0514e47fc3fd47b30313
mark-marshall <mark.marshallgp@gmail.com>
2019-06-16 11:18:27 +0100

    Implemented 1-sec first pass solution on names

--- names/names.py
+++ names/names.py
@@ -10,11 +10,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Provided solution
+# Runtime = 4.6 seconds
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# First pass solution using Python in-built "in" function (from C so much faster)
+# Runtime = 1.0 seconds
 duplicates = []
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")

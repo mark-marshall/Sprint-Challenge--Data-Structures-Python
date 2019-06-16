8e811b977343fd9df12bcbceebfc8903e9d9f0f2
mark-marshall <mark.marshallgp@gmail.com>
2019-06-16 11:26:20 +0100

    Implemented binary search solution for names

--- names/names.py
+++ names/names.py
@@ -27,10 +27,18 @@ f.close()
 #         duplicates.append(name_1)
 
 # Second pass solution using binary search tree data structure
-#  Runtime = 
-
+#  Runtime = 0.1 seconds
+duplicates = []
+# Initialise the search tree with letter m to give a better chance of an even tree
+comparison_tree = BinarySearchTree('m')
+# insert each name from names_2 into the binary search tree
+for name_2 in names_2:
+    comparison_tree.insert(name_2)
+# loop over names_1 and use the contains method on comparison_tree to check for duplicates
+for name_1 in names_1:
+    if comparison_tree.contains(name_1):
+        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-

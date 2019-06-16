4a7a1970398e31d02d645039374ad66c55757456
mark-marshall <mark.marshallgp@gmail.com>
2019-06-16 11:10:43 +0100

    Completed append method on ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,24 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
-
+    # check if the storage contains any Nones and replace first available None
+    if None in self.storage:
+      # get index of first None
+      append_index = self.storge.index(None)
+      # add new item at the append_index
+      self.storage[append_index] = item
+    # if there are no Nones, find the oldest item and replace it
+    else:
+      # initialize the oldest item as the start of the list
+      oldest_item = self.storage[0]
+      # loop through the list to find the oldest item
+      for item_i in self.storage:
+        if item_i < oldest_item:
+          oldest_item = item_i
+      # get the index of the oldest item
+      append_index = self.storage.index(oldest_item)
+      # add new item at the append_index
+      self.storage[append_index] = item
+    
   def get(self):
     pass
